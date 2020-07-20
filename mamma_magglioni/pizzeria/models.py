7a5d53731f181134c1e8406a5b0f18dbd6602a7f
Amelia Brito <ameliagiampaolo@gmail.com>
2020-07-20 14:20:19 -0400

    arreglando detales en los modelos y creando migraciones

--- mamma_magglioni/pizzeria/models.py
+++ mamma_magglioni/pizzeria/models.py
@@ -2,28 +2,28 @@ from django.db import models
 
 class Cliente(models.Model):
     id = models.IntegerField(primary_key=True)
-    nombre  models.CharField(max_length=15)
+    nombre = models.CharField(max_length=15)
     apellido = models.CharField(max_length=20)
 
 class Pedido(models.Model):
     id = models.IntegerField(primary_key=True)
     fecha = models.DateField('fecha del pedido')
-    precio_total = models.DecimalField(decimal_places=2)
+    precio_total = models.DecimalField(max_digits=4, decimal_places=2)
     fk_cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
+
+class Ingrediente(models.Model):
+    id = models.IntegerField(primary_key=True)
+    nombre = models.CharField(max_length=12)
+    precio = models.DecimalField(max_digits=4, decimal_places=2)
 
 class Pizza(models.Model):
     id = models.IntegerField(primary_key=True)
-    tamano  = models.CharField(max_length=7)
-    precio = models.IntegerField(decimal_places=2)
+    size  = models.CharField(max_length=9)
+    precio = models.IntegerField()
     adicionales = models.ManyToManyField(
         Ingrediente,
         through='Pizza_ing'
     )
-
-class Ingrediente(models.Model):
-    id = models.IntegerField(primary_key=True)
-    nombre = models.CharField(max_length=11)
-    precio = models.DecimalField(decimal_places=2)
 
 class Pizza_ing(models.Model):
     id = models.IntegerField(primary_key=True)
