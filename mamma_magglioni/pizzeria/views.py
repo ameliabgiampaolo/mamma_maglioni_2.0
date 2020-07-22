d8ea2412bd97e2f99049375e46c6aba02177c6b6
vmgarcia18 <vmgarcia18@hotmail.com>
2020-07-22 13:13:50 -0400

    Plantilla index con bootstrap

--- mamma_magglioni/pizzeria/views.py
+++ mamma_magglioni/pizzeria/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse('Hello World!')
+    context = {}
+    return render(request, 'pizzeria/index.html', context)
