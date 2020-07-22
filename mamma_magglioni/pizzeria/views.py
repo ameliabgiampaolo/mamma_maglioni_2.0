2f8a05717c2d494342f1c1ca6f3adccab0e372cb
Amelia Brito <ameliagiampaolo@gmail.com>
2020-07-22 14:06:38 -0400

    imagenes de ingredientes y eliminar archivos innecesarios

--- mamma_magglioni/pizzeria/views.py
+++ mamma_magglioni/pizzeria/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    context = {}
+    context = {} 
     return render(request, 'pizzeria/index.html', context)
