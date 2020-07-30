aadbb5d2921dac47a4905575a04a335e3fc1a6c8
Rafael <rouser4345@gmail.com>
2020-07-30 19:40:40 -0400

    Filtros del listado de Pizza_Ing Admin

--- mamma_magglioni/pizzeria/admin.py
+++ mamma_magglioni/pizzeria/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
-
 from .models import Cliente, Pedido, Ingrediente, Pizza, Pizza_ing
+
+class Pizza_ingAdmin(admin.ModelAdmin):
+    list_display  =  ( 'fk_pizza', 'fk_ingrediente', 'fk_pedido', 'num_pizza',  )
+    list_filter = ['fk_pizza', 'fk_ingrediente','fk_pedido' ]
+    search_fields = ['fk_pedido']
+    list_per_page = 10
 
 admin.site.register(Cliente)
 admin.site.register(Pedido)
 admin.site.register(Ingrediente)
 admin.site.register(Pizza)
-admin.site.register(Pizza_ing)
+admin.site.register(Pizza_ing, Pizza_ingAdmin)
