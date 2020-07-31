b532c404cb447889f137c5a532305db77aeaa60b
vmgarcia18 <vmgarcia18@hotmail.com>
2020-07-31 18:28:43 -0400

    Modificando index y fecha en resumen

--- mamma_magglioni/pizzeria/views.py
+++ mamma_magglioni/pizzeria/views.py
@@ -46,46 +46,12 @@ def order(request, cliente_id, pizzas):
 
     return render(request, 'pizzeria/order_form.html', { 'formset': formset })
 
-"""def order(request):
-    ingrediente = False
-    now = datetime.date.today()
-    if request.method == 'POST':
-        form_pizza = OrderPizzaForm(request.POST)
-        form_ingrediente = OrderIngredienteForm(request.POST)
-        form_client = ClientForm(request.POST)
-
-        if form_pizza.is_valid():
-            pizzas = form_pizza.cleaned_data.get('pizzas')
-        
-        if form_ingrediente.is_valid():
-            ingredientes = form_ingrediente.cleaned_data.get('ingredientes')
-            ingrediente = True
-
-        if form_client.is_valid():
-            cliente = form_client.save() 
-
-
-        pedido = Pedido.objects.create_pedido(now, cliente)
-
-        if ingrediente == True:
-            for i in range(len(ingredientes)):
-                pizza_ing = Pizza_ing.objects.create_pizza_ing(Pizza.objects.get(id=pizzas), Ingrediente.objects.get(id=ingredientes[i]), pedido)
-        else:
-            pizza_ing = Pizza_ing.objects.create_pizza_ing(Pizza.objects.get(id=pizzas),None,pedido)
-
-        return redirect('resumen', pedido.id)
-    else:
-        form_pizza = OrderPizzaForm
-        form_ingrediente = OrderIngredienteForm
-        form_client = ClientForm
-
-    return render(request, 'pizzeria/order_form.html', { 'form':form_pizza, 'form2':form_ingrediente, 'form3': form_client })"""
-
 def resumen(request, pedido_id):
     pedido = Pedido.objects.get(id=pedido_id)
+    date = pedido.fecha.strftime("%d-%m-%Y")
     cliente = Cliente.objects.get(id=pedido.fk_cliente_id)
     pizza_ing = Pizza_ing.objects.filter(fk_pedido_id=pedido_id) #filter lo uso para traer varios registros de la bd, varias pizza_ing con ese pedido
-    context = {'cliente': cliente, 'pedido': pedido, 'pizza_ing': pizza_ing}
+    context = {'cliente': cliente, 'pedido': pedido, 'pizza_ing': pizza_ing, 'date': date}
     return render(request, 'pizzeria/resumen.html', context)
 
 def finalizar(request):
