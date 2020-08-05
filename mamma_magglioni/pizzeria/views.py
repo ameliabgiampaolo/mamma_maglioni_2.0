f3ff683946b4851415ead36bdb02632f021b6f55
Neradkiller <daren1997gonzalez@gmail.com>
2020-08-05 14:49:09 -0400

    Reporte 5 actualizado

--- mamma_magglioni/pizzeria/views.py
+++ mamma_magglioni/pizzeria/views.py
@@ -126,7 +126,11 @@ def reporte4(request):
     return render(request,'pizzeria/reporte4.html',context)
 
 def reporte5(request):
-    nombres = Pedido.objects.distinct()
-    pedidos = Pizza_ing.objects.all()
+    nombres = Cliente.objects.values('nombre','apellido').distinct()
+    pedidos = Pedido.objects.all()
+    for nombre in nombres:
+        print(nombre)
+    for pedido in pedidos:
+        print(pedido)
     context = {'nombres': nombres,'pedidos': pedidos}
     return render(request,'pizzeria/reporte5.html', context)
